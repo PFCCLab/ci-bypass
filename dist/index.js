13e4938d655393b5c59ccefd49401d9a4a8cd106
SigureMo <sigure.qaq@gmail.com>
2025-01-03 02:47:55 +0800

    🔥 chore: cleanup unused tools for me

--- dist/index.js
+++ dist/index.js
@@ -25716,7 +25716,7 @@ exports.wait = wait;
  * @returns {Promise<string>} Resolves with 'done!' after the wait is over.
  */
 async function wait(milliseconds) {
-    return new Promise(resolve => {
+    return new Promise((resolve) => {
         if (isNaN(milliseconds)) {
             throw new Error('milliseconds not a number');
         }
